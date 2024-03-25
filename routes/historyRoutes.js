5d760dac676af4c5075a33355926af066c72dd31
DanySA22 <winache22@gmail.com>
2024-03-25 13:23:04 -0700

    Reorganize the routes files

--- routes/historyRoutes.js
+++ routes/historyRoutes.js
@@ -5,14 +5,12 @@ const OutputUser = require('../models/outputUser')
 const passport = require('passport')
 require('../controllers/passport')
 
-//this route pull from MongoDB based in user id
+//this route pull from MongoDB based in authenticated used
 router.get('/', passport.authenticate('jwt', {session:false}), async (req, res) => {
     try {
         const userID = req.user.userId //passport middleware construct a user object from the decoded payload 
         //and a attached to the req object. This user object include info about the user id,e tc that we add when we sign
         //the JWT
-        //because we are using this approach we don't need to use the dynamic id on endpoint
-        
         const outputLatest = await OutputUser.find({userId: userID})
         res.json(outputLatest)
 } catch (error) {
